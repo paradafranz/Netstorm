1a1ae3f894d4db4cbb8e569dc1bb9c7f2801a8a8
Charles Dyniel <libotcharles@itis-molinari.eu>
2025-10-22 13:53:32 +0200

    aggiornamento view

--- view.py
+++ view.py
@@ -1,8 +1,26 @@
-def main:
-catalogo = Catalogo()
+def menu():
+    while True:
+        print("=== MENU LIBRERIA ===")
+        print("1. Inserisci libro")
+        print("2. Ricerca per titolo")
+        print("3. Ricerca per autore")
+        print("4. Ricerca per ISBN")
+        print("5. Esci")
 
-print("--- 1. INSERIMENTO DEI DATI ---")
+        scelta = input("Scegli un'opzione: ")
 
-catalogo.aggiungi_libro("dsadsadsa", "8392138", 1840, "marco", "dshadjsa")
-    catalogo.aggiungi_libro("dbahjda", "3219031230912", 1851, "dasdsa", "dsadsanjda")
-    catalogo.aggiungi_libro("dahsjda", "32931", 2020, "dsa", "dsadsadsa")
+        if scelta == "1":
+            inserisci_libro()
+        elif scelta == "2":
+            ricerca_per_titolo()
+        elif scelta == "3":
+            ricerca_per_autore()
+        elif scelta == "4":
+            ricerca_per_isbn()
+        elif scelta == "5":
+            print("Uscita dal programma.")
+            break
+        else:
+            print("Scelta non valida, riprova.\n")
+
+menu()
