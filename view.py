a485d8a7530f721ecbbd694d6f7ed3648f935f31
Charles Dyniel <libotcharles@itis-molinari.eu>
2025-10-24 10:31:20 +0200

    codice view

--- view.py
+++ view.py
@@ -1,26 +1,11 @@
-def menu():
-    while True:
-        print("=== MENU LIBRERIA ===")
-        print("1. Inserisci libro")
-        print("2. Ricerca per titolo")
-        print("3. Ricerca per autore")
-        print("4. Ricerca per ISBN")
-        print("5. Esci")
+def libro_inserito():
+    print("Libro inserito con successo!\n")
 
-        scelta = input("Scegli un'opzione: ")
-
-        if scelta == "1":
-            inserisci_libro()
-        elif scelta == "2":
-            ricerca_per_titolo()
-        elif scelta == "3":
-            ricerca_per_autore()
-        elif scelta == "4":
-            ricerca_per_isbn()
-        elif scelta == "5":
-            print("Uscita dal programma.")
-            break
-        else:
-            print("Scelta non valida, riprova.\n")
-
-menu()
+def mostra_risultati(risultati):
+    if risultati:
+        print("\n--- RISULTATI ---")
+        for libro in risultati:
+            print(f"Titolo: {libro['titolo']}, Autore: {libro['autore']}, ISBN: {libro['isbn']}")
+    else:
+        print("Nessun libro trovato.")
+    print()
