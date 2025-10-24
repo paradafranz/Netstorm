880cabdc206526c48cb8d09b86cf5cc8de040f0e
Charles Dyniel <libotcharles@itis-molinari.eu>
2025-10-24 10:48:42 +0200

    view aggiornato

--- view.py
+++ view.py
@@ -1,11 +1,8 @@
-def libro_inserito():
-    print("Libro inserito con successo!\n")
-
 def mostra_risultati(risultati):
     if risultati:
         print("\n--- RISULTATI ---")
         for libro in risultati:
-            print(f"Titolo: {libro['titolo']}, Autore: {libro['autore']}, ISBN: {libro['isbn']}")
+            print(libro)  
     else:
         print("Nessun libro trovato.")
     print()
