18d3b8078ee122c30a5bedef379e402fb5f39c96
paradafranz <paradafranz@itis-molinari.eu>
2025-10-26 12:16:24 +0100

    Update model.py

--- model.py
+++ model.py
@@ -1,16 +1,1 @@
-class Libro:
-    def __init__(self, titolo, isbn, autore):
-        self.titolo = titolo
-        self.isbn = isbn
-        self.autore = autore 
 
-    def __str__(self):
-        return f"Titolo: {self.titolo}, ISBN: {self.isbn}, Autore: {self.autore.nome}"
-
-class Autore:
-    def __init__(self, nome, bio=""):
-        self.nome = nome
-        self.bio = bio
-
-    def __str__(self):
-        return f"{self.nome} - {self.bio}"
